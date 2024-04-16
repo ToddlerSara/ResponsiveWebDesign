ce668ee3b21a1ac0b05160684f08c5fa5b04c630
ToddlerSara <160302245+ToddlerSara@users.noreply.github.com>
2024-04-17 00:14:40 +0800

    頁面粗刻定案
    先依照設計圖刻畫，之後再細修

--- ResaturantMeau/src/App.vue
+++ ResaturantMeau/src/App.vue
@@ -9,21 +9,30 @@ const RestaurantInfor = {
 
 <template>
   <div id="app">
-    <header>
-      <div class="restaurant-name"><router-link to="/">{{ RestaurantInfor.name }}</router-link></div>
+    <div class="header">
       <div class="restaurant-info">
-        <div class="info-item"><router-link to="/contact">特色</router-link></div>
-        <div class="info-item">主廚</div>
-        <div class="info-item"><router-link to="/menu">Menu</router-link></div>
+        <div class="restaurant-name"><router-link to="/">{{ RestaurantInfor.name }}</router-link></div>
+        <div class="info-item">
+          <div class="info"><router-link to="/contact">特色</router-link></div>
+          <div class="info">主廚</div>
+          <div class="info"><router-link to="/menu">Menu</router-link></div>
+        </div>
       </div>
-    </header>
-    <router-view></router-view>
+      <div>
+        <div>咬一口就無法忘懷的漢堡</div>
+        <div>七夕絕對無法錯過的好滋味
+          六角大漢堡 </div>
+      </div>
+    </div>
+    <div class="body">
+      <router-view></router-view>
+    </div>
     <footer>
       <div class="contact-info">
         <div>Email: example@example.com</div>
         <div>Phone: 123-456-7890 (Monday to Friday 9:30-18:00, except holidays)</div>
       </div>
-      <div class="contact-name">
+      <div class="footer-center">
         <div>&copy; {{ RestaurantInfor.name }}</div>
       </div>
     </footer>
@@ -31,35 +40,49 @@ const RestaurantInfor = {
 </template>
 
 <style lang="scss" scoped>
-header {
-  position: fixed;
-  top: 0;
-  left: 0;
+.header {
   width: 100%;
-  background-color: #fff;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  height: 420px;
+  opacity: 100%;
+  background-color: #333;
   padding: 10px 20px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  font-family: "Noto Sans TC", sans-serif;
+  color: #FFFFFF;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-wrap: wrap;
 }
 
 .restaurant-info {
+  position: absolute;
+  top: 0;
   display: flex;
+  width: 1024px;
+  height: 56px;
+  justify-content: space-between;
+}
+
+.restaurant-name {
+  font-optical-sizing: auto;
+  font-weight: bold;
+  font-style: normal;
+  font-size: 24px;
 }
 
 .info-item {
   margin-right: 20px;
+  display: flex;
+  font-size: 16px;
+  justify-content: space-between;
 }
 
-nav {
-  margin-top: 50px;
-  /* To offset the fixed header */
-
+.body {
+  position: relative;
 }
 
 footer {
-  position: fixed;
   bottom: 0;
   left: 0;
   width: 100%;
@@ -77,7 +100,7 @@ footer {
   margin-bottom: 5px;
 }
 
-.contact-name {
+.footer-center {
   text-align: center;
 }
 
