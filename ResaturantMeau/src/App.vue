1a45620b6aecf9838c08e06be32214784c8d97b4
ToddlerSara <160302245+ToddlerSara@users.noreply.github.com>
2024-04-17 17:22:12 +0800

    文字框居中

--- ResaturantMeau/src/App.vue
+++ ResaturantMeau/src/App.vue
@@ -18,10 +18,16 @@ const RestaurantInfor = {
           <div class="info"><router-link to="/menu">Menu</router-link></div>
         </div>
       </div>
-      <div>
-        <div>咬一口就無法忘懷的漢堡</div>
-        <div>七夕絕對無法錯過的好滋味
-          六角大漢堡 </div>
+      <div class="restaurant-describe">
+        <div class="describe-head">咬一口就無法忘懷的漢堡</div>
+        <link rel="stylesheet" href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.css">
+        <div class="describe-body">
+          <div>七夕絕對無法錯過的好滋味
+            <i class="fa fa-cutlery" style="font-size:16px"></i>
+            六角大漢堡
+            <i class="fa fa-cutlery" style="font-size:16px"></i>
+          </div>
+        </div>
       </div>
     </div>
     <div class="body">
@@ -45,6 +51,9 @@ const RestaurantInfor = {
   height: 420px;
   opacity: 100%;
   background-color: #333;
+  background-image: url(https://hexschool.github.io/Rwd_sketch_export/assets/header_desktop@2x.png);
+  background-repeat: no-repeat;
+  background-size: contain;
   padding: 10px 20px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   font-family: "Noto Sans TC", sans-serif;
@@ -69,6 +78,29 @@ const RestaurantInfor = {
   font-weight: bold;
   font-style: normal;
   font-size: 24px;
+  padding: 16px 42px;
+}
+
+.restaurant-describe {
+  background: rgba(0, 0, 0, 0.35);
+  width: 460px;
+  height: 152px;
+}
+
+.describe-head {
+  width: 100%;
+  padding: 20px 0 16px 0;
+  text-align: center;
+  font-family: 微軟正黑體;
+  font-size: 36px;
+}
+
+.describe-body {
+  width: 192px;
+  height: 42px;
+  align-content: center;
+  text-align: center;
+  margin: auto;
 }
 
 .info-item {
@@ -76,6 +108,11 @@ const RestaurantInfor = {
   display: flex;
   font-size: 16px;
   justify-content: space-between;
+  align-content: center;
+}
+
+.info {
+  padding: 18px 31px;
 }
 
 .body {
